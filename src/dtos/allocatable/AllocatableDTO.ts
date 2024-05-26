84139b1d9845774ee392593318b11c4627402e8f
ingrid <iroliveira@viptech.com.br>
2024-05-26 14:48:00 -0300

    fix(Allocatable): send all informatins on get one allocatable

--- src/dtos/allocatable/AllocatableDTO.ts
+++ src/dtos/allocatable/AllocatableDTO.ts
@@ -3,23 +3,29 @@ import { Block } from '../../entities/BlockEntity';
 import { ItemsAllocatable } from './itemsAllocatable/ItemsAllocatableDTO';
 
 export class AllocatableDTO {
+  id: number;
   name: string;
   typeId: number;
   blockId: number;
   isDisabled: boolean;
-  items: ItemsAllocatable;
+  createdAt: Date;
+  updatedAt: Date;
   blockName?: string;
+  items: ItemsAllocatable;
 
   constructor(
     allocatable: Allocatable,
     items: ItemsAllocatable,
     block?: Block,
   ) {
+    this.id = allocatable.id;
     this.name = allocatable.name;
     this.typeId = allocatable.typeId;
     this.blockId = allocatable.blockId;
     this.isDisabled = allocatable.isDisabled;
-    this.items = items;
     this.blockName = block?.name;
+    this.createdAt = allocatable.createdAt;
+    this.updatedAt = allocatable.updatedAt;
+    this.items = items;
   }
 }
